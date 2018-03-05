09d18628d570dc9cc5618c1372069d459002fd56
Matilda Rutherford <rutherford.matilda@gmail.com>
2018-03-06 10:59:56 +1100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped" + " " + cardsInPlay[0]);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped" + " " + cardsInPlay[1]);
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	alert ("You found a match!")
+}
+else (
+	alert ("Sorry, try again"));
