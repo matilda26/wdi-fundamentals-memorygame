5b3ec4064509b1aadebd43bc7f170c9ad4a13cf3
Matilda Rutherford <rutherford.matilda@gmail.com>
2018-03-09 11:55:56 +1100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
 console.log("Up and running!");
 
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+rank: 'queen',
+suit: 'hearts',
+cardImage: 'images/queen-of-hearts.png'
+},
+{
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: 'images/queen-of-diamonds.png'
+}, 
+{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: 'images/king-of-hearts.png'
+},
+{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: 'images/king-of-diamonds.png'
+}
+	];
+
+
 var cardsInPlay = [];
 
 var checkForMatch = function () {
@@ -14,10 +37,14 @@ alert("Sorry, try again.");
 
 
 var flipCard = function (cardID) {
-console.log ("User flipped " + cards[cardID]);
-cardsInPlay.push(cards[cardID]);
+console.log ("User flipped " + cards[cardID].rank);
+cardsInPlay.push(cards[cardID].rank);
+console.log(cards[cardID].cardImage);
+console.log(cards[cardID].suit);
 checkForMatch();
 };
 
 flipCard (0);
 flipCard (2);
+
+
